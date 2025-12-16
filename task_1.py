7fb734c4392d80cad54c7c7a30e76a32085d3264
Oleksandr Kozlenko <stupidahhhit@gmail.com>
2025-12-16 15:17:04 +0200

    Впиравленна помилка з роками

--- task_1.py
+++ task_1.py
@@ -10,7 +10,7 @@ book_profile ={
 print(book_profile["title"], book_profile["author"])
 print(book_profile["publisher_info"]["name"])
 print("Книга '",book_profile["title"],"' автора",book_profile["author"],"УБла видана у місті", book_profile["publisher_info"]["city"])
-if book_profile.get("year")!="None":
+if book_profile.get("year")!= None :
     print(book_profile["year"])
 else:
     print("Рік видання невідомий")
